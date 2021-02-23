14fae42b486d2643c9cf81e63cfa914f2f8ba5e2
SpencerPearson <spencer.pearson.816@outlook.com>
2021-02-23 16:31:35 -0600

    update to discord.py requires user intents to be loaded. added user intents and updated permissions on the bot

--- sppd-card-level-tracker.py
+++ sppd-card-level-tracker.py
@@ -1,4 +1,4 @@
-# bot.py
+# sppd-card-level-tracker.py
 # this is a discord bot written to keep track of team members' card levels in SPPD
 import os
 
@@ -7,11 +7,28 @@ from dotenv import load_dotenv
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
+GUILD = os.getenv('DISCORD_GUILD')
 
-client = discord.Client()
+intents = discord.Intents.all()
+client = discord.Client(intents=intents)
 
 @client.event
 async def on_ready():
-    print(f'{client.user} has connected to Discord!')
+    guild = discord.utils.get(client.guilds, name=GUILD)
+
+    print(
+        f'{client.user} is connected to the following guild:\n'
+        f'{guild.name}(id: {guild.id})\n'
+    )
+
+    members = '\n - '.join([member.name for member in guild.members])
+    print(f'Guild Members:\n {members}')
+
+@client.event
+async def on_member_join(member):
+    await member.create_dm()
+    await member.dm_channel.send(
+        f'Hi {member.name}, welcome to my Discord server!'
+    )
 
 client.run(TOKEN)
